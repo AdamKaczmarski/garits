d72fbfd683a3a5bc1b4aa37c50e1811e157a03dd
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-02-22 18:34:35 +0000

    Added DB DDL and a queries file for the task

---
+++ garits_db/queries_first_deliverable.sql
@@ -0,0 +1,20 @@
+-- This file contains queries for the ER Diagram required in the first deliverable of the project. (2 Selects, 2 Inserts, 2 Updates, 2 Deletes and 2 Reporting queries based on the scenario)
+-- Select 1
+
+-- Select 2
+
+-- Update 1
+
+-- Update 2
+
+-- Insert 1
+
+-- Insert 2
+
+-- Delete 1
+
+-- Delete 2
+
+-- Report 1 (Job Sheet)
+
+-- Report 2 (Spare Parts / Stock Level)
