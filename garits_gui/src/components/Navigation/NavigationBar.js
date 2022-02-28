7af4662dbaf884c921a736b1b2b40c5585eb49a1
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-02-28 22:46:06 +0000

    dummy data models

--- garits_gui/src/components/Navigation/NavigationBar.js
+++ garits_gui/src/components/Navigation/NavigationBar.js
@@ -2,10 +2,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 /*need to add conditions what should be shown for each role */
-
 
 const NavigationBar = () => {
   return (
@@ -15,22 +14,34 @@ const NavigationBar = () => {
       variant="dark"
       className="w-100 "
       expand="md"
-      style={{ boxShadow: "0px 0 5px rgba(0, 0, 0, 0.5)", borderRadius:"6px 6px 6px 6px" }}
+      style={{
+        boxShadow: "0px 0 5px rgba(0, 0, 0, 0.5)",
+        borderRadius: "6px 6px 6px 6px",
+      }}
     >
-     
       <Container fluid>
-        <Navbar.Brand href="#/">GARITS                </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbar-nav"/>
-            
-            <Navbar.Collapse id="navbar-nav">
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link as={Link} to="/">Link</Nav.Link>
-                <Nav.Link as={Link} to="jobs">Jobs</Nav.Link>
-                <Nav.Link as={Link} to="inventory">Inventory</Nav.Link>
-                <Nav.Link as={Link} to="users">Users</Nav.Link>
-                <Nav.Link as={Link} to="login">Login/out(TBD)</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
+        <Navbar.Brand href="#/">GARITS </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav.Link as={Link} to="jobs">
+              Jobs
+            </Nav.Link>
+            <Nav.Link as={Link} to="customers">
+              Customers
+            </Nav.Link>
+            <Nav.Link as={Link} to="inventory">
+              Inventory
+            </Nav.Link>
+            <Nav.Link as={Link} to="users">
+              Users
+            </Nav.Link>
+            <Nav.Link as={Link} to="login">
+              Login/out(TBD)
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
---
+++ garits_gui/src/dummy-data/customers.js
@@ -0,0 +1,16 @@
+export const customers = [
+  {
+    id: 1,
+    name: "Company 1",
+    address: "Street 1",
+    postcode: "111E22",
+    telephone_bumer: "1234567890",
+    email: "company@test.com",
+    fax: null,
+    fixed_discount: 0,
+    flex_discount: [
+      { rangeFrom: 10000, discount: 5 },
+      { rangeFrom: 20000, discount: 10 },
+    ],
+  },
+];
