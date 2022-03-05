45962a874a0a804260737b0f1d538ae8e82d60a9
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-03-05 20:25:50 +0000

    schema updates and first report

--- garits_db/garits.sql
+++ garits_db/garits.sql
@@ -1,384 +1,390 @@
 ALTER TABLE
-    Users_Roles DROP FOREIGN KEY FKUsers_Role251675;
+    `Users_Roles` DROP FOREIGN KEY `FKUsers_Role251675`;
 
 ALTER TABLE
-    Users_Roles DROP FOREIGN KEY FKUsers_Role790442;
+    `Users_Roles` DROP FOREIGN KEY `FKUsers_Role790442`;
 
 ALTER TABLE
-    Jobs_Customers DROP FOREIGN KEY FKJobs_Custo162114;
+    `Jobs_Customers` DROP FOREIGN KEY `FKJobs_Custo162114`;
 
 ALTER TABLE
-    Jobs_Customers DROP FOREIGN KEY FKJobs_Custo598651;
+    `Jobs_Customers` DROP FOREIGN KEY `FKJobs_Custo598651`;
 
 ALTER TABLE
-    Users_Jobs DROP FOREIGN KEY FKUsers_Jobs928384;
+    `Users_Jobs` DROP FOREIGN KEY `FKUsers_Jobs928384`;
 
 ALTER TABLE
-    Users_Jobs DROP FOREIGN KEY FKUsers_Jobs91376;
+    `Users_Jobs` DROP FOREIGN KEY `FKUsers_Jobs91376`;
 
 ALTER TABLE
-    Jobs_Services DROP FOREIGN KEY FKJobs_Servi182926;
+    `Jobs_Services` DROP FOREIGN KEY `FKJobs_Servi182926`;
 
 ALTER TABLE
-    Jobs_Services DROP FOREIGN KEY FKJobs_Servi475550;
+    `Jobs_Services` DROP FOREIGN KEY `FKJobs_Servi475550`;
 
 ALTER TABLE
-    Jobs_Parts DROP FOREIGN KEY FKJobs_Parts634881;
+    `Jobs_Parts` DROP FOREIGN KEY `FKJobs_Parts634881`;
 
 ALTER TABLE
-    Jobs_Parts DROP FOREIGN KEY FKJobs_Parts55459;
+    `Jobs_Parts` DROP FOREIGN KEY `FKJobs_Parts55459`;
 
 ALTER TABLE
-    Customers_Vehicles DROP FOREIGN KEY FKCustomers_94687;
+    `Customers_Vehicles` DROP FOREIGN KEY `FKCustomers_94687`;
 
 ALTER TABLE
-    Customers_Vehicles DROP FOREIGN KEY FKCustomers_547421;
+    `Customers_Vehicles` DROP FOREIGN KEY `FKCustomers_547421`;
 
 ALTER TABLE
-    Parts_RetailSales DROP FOREIGN KEY FKParts_Reta997959;
+    `Payments_Customer` DROP FOREIGN KEY `FKPayments_C594261`;
 
 ALTER TABLE
-    Parts_RetailSales DROP FOREIGN KEY FKParts_Reta600423;
+    `Payments_Customer` DROP FOREIGN KEY `FKPayments_C52864`;
 
 ALTER TABLE
-    Payments_Customer DROP FOREIGN KEY FKPayments_C594261;
+    `Parts_Orders` DROP FOREIGN KEY `FKParts_Orde7122`;
 
 ALTER TABLE
-    Payments_Customer DROP FOREIGN KEY FKPayments_C52864;
+    `Parts_Orders` DROP FOREIGN KEY `FKParts_Orde56004`;
 
 ALTER TABLE
-    Jobs DROP FOREIGN KEY FKJobs204816;
+    `Jobs` DROP FOREIGN KEY `FKJobs210684`;
 
 ALTER TABLE
-    Parts_Orders DROP FOREIGN KEY FKParts_Orde7122;
+    `Customer_Variable_Discounts_Services` DROP FOREIGN KEY `FKCustomer_V790173`;
 
 ALTER TABLE
-    Parts_Orders DROP FOREIGN KEY FKParts_Orde56004;
+    `Customer_Variable_Discounts_Services` DROP FOREIGN KEY `FKCustomer_V539825`;
 
 ALTER TABLE
-    Jobs DROP FOREIGN KEY FKJobs638019;
+    `Jobs_Payments` DROP FOREIGN KEY `FKJobs_Payme844591`;
 
 ALTER TABLE
-    Customer_Variable_Discounts_Services DROP FOREIGN KEY FKCustomer_V790173;
+    `Jobs_Payments` DROP FOREIGN KEY `FKJobs_Payme733948`;
 
 ALTER TABLE
-    Customer_Variable_Discounts_Services DROP FOREIGN KEY FKCustomer_V539825;
+    `Parts_Payments` DROP FOREIGN KEY `FKParts_Paym554541`;
 
-DROP TABLE IF EXISTS Users;
+ALTER TABLE
+    `Parts_Payments` DROP FOREIGN KEY `FKParts_Paym813726`;
 
-DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS `Users`;
 
-DROP TABLE IF EXISTS Users_Roles;
+DROP TABLE IF EXISTS `Roles`;
 
-DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS `Users_Roles`;
 
-DROP TABLE IF EXISTS Jobs;
+DROP TABLE IF EXISTS `Customers`;
 
-DROP TABLE IF EXISTS Parts;
+DROP TABLE IF EXISTS `Jobs`;
 
-DROP TABLE IF EXISTS Vehicles;
+DROP TABLE IF EXISTS `Parts`;
 
-DROP TABLE IF EXISTS Jobs_Customers;
+DROP TABLE IF EXISTS `Vehicles`;
 
-DROP TABLE IF EXISTS Users_Jobs;
+DROP TABLE IF EXISTS `Jobs_Customers`;
 
-DROP TABLE IF EXISTS Services;
+DROP TABLE IF EXISTS `Users_Jobs`;
 
-DROP TABLE IF EXISTS Jobs_Services;
+DROP TABLE IF EXISTS `Services`;
 
-DROP TABLE IF EXISTS Jobs_Parts;
+DROP TABLE IF EXISTS `Jobs_Services`;
 
-DROP TABLE IF EXISTS Customers_Vehicles;
+DROP TABLE IF EXISTS `Jobs_Parts`;
 
-DROP TABLE IF EXISTS RetailSales;
+DROP TABLE IF EXISTS `Customers_Vehicles`;
 
-DROP TABLE IF EXISTS Parts_RetailSales;
+DROP TABLE IF EXISTS `Parts_Payments`;
 
-DROP TABLE IF EXISTS Payments_Customer;
+DROP TABLE IF EXISTS `Payments_Customer`;
 
-DROP TABLE IF EXISTS Payments;
+DROP TABLE IF EXISTS `Payments`;
 
-DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS `Orders`;
 
-DROP TABLE IF EXISTS Parts_Orders;
+DROP TABLE IF EXISTS `Parts_Orders`;
 
-DROP TABLE IF EXISTS Customer_Variable_Discounts_Services;
+DROP TABLE IF EXISTS `Customer_Variable_Discounts_Services`;
 
-CREATE TABLE Users (
-    id_user int(11) NOT NULL AUTO_INCREMENT,
-    email varchar(100) NOT NULL UNIQUE,
-    password varchar(32) NOT NULL,
-    salt varchar(32) NOT NULL,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(100) NOT NULL,
-    PRIMARY KEY (id_user),
-    UNIQUE INDEX (id_user)
+DROP TABLE IF EXISTS `Jobs_Payments`;
+
+CREATE TABLE `Users` (
+    `id_user` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(100) NOT NULL UNIQUE,
+    `password` varchar(32) NOT NULL,
+    `salt` varchar(32) NOT NULL,
+    `first_name` varchar(50) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    PRIMARY KEY (`id_user`),
+    UNIQUE INDEX (`id_user`)
 );
 
-CREATE TABLE Roles (
-    id_role int(11) NOT NULL AUTO_INCREMENT,
-    role_name varchar(20) NOT NULL,
-    PRIMARY KEY (id_role),
-    UNIQUE INDEX (id_role)
+CREATE TABLE `Roles` (
+    `id_role` int NOT NULL AUTO_INCREMENT,
+    `role_name` varchar(20) NOT NULL,
+    PRIMARY KEY (`id_role`),
+    UNIQUE INDEX (`id_role`)
 );
 
-CREATE TABLE Users_Roles (
-    user_id int(11) NOT NULL,
-    role_id int(11) NOT NULL
+CREATE TABLE `Users_Roles` (
+    `user_id` int NOT NULL,
+    `role_id` int NOT NULL
 );
 
-CREATE TABLE Customers (
-    id_customer int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
-    city varchar(50) NOT NULL,
-    address varchar(150) NOT NULL,
-    postcode varchar(10) NOT NULL,
-    telephone_number int(11) NOT NULL UNIQUE,
-    email varchar(100) NOT NULL UNIQUE,
-    fax int(12),
-    fixed_discount int(2),
-    flex_discounts_json text,
-    PRIMARY KEY (id_customer),
-    UNIQUE INDEX (id_customer)
+CREATE TABLE `Customers` (
+    `id_customer` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    `city` varchar(50) NOT NULL,
+    `address` varchar(150) NOT NULL,
+    `postcode` varchar(10) NOT NULL,
+    `telephone_number` int NOT NULL UNIQUE,
+    `email` varchar(100) NOT NULL UNIQUE,
+    `fax` int,
+    `fixed_discount` int,
+    `flex_discounts_json` text,
+    PRIMARY KEY (`id_customer`),
+    UNIQUE INDEX (`id_customer`)
 );
 
-CREATE TABLE Jobs (
-    id_job int(11) NOT NULL AUTO_INCREMENT,
-    payment_id int(10) NOT NULL,
-    no_reg_id varchar(10) NOT NULL,
-    description_done varchar(2000),
-    description_required varchar(2000),
-    est_time_min int(4) NOT NULL,
-    act_time_min int(4) NOT NULL,
-    create_date timestamp NOT NULL,
-    booking_date datetime NULL,
-    fix_date datetime NULL,
-	bay varchar(30) NULL,
-    PRIMARY KEY (id_job),
-    UNIQUE INDEX (id_job)
+CREATE TABLE `Jobs` (
+    `id_job` int NOT NULL AUTO_INCREMENT,
+    `reg_no_id` varchar(10) NOT NULL,
+    `status` varchar(50) NOT NULL,
+    `description_done` varchar(2000),
+    `description_required` varchar(2000),
+    `est_time_min` int NOT NULL,
+    `act_time_min` int NOT NULL,
+    `create_date` timestamp NOT NULL,
+    `booking_date` date NULL,
+    `fix_date` date NULL,
+    `bay` varchar(30),
+    PRIMARY KEY (`id_job`),
+    UNIQUE INDEX (`id_job`)
 );
 
-CREATE TABLE Parts (
-    id_part int(11) NOT NULL AUTO_INCREMENT,
-    part_name varchar(200) NOT NULL,
-    part_type varchar(20),
-    code varchar(25) NOT NULL,
-    manufacturer varchar(20) NOT NULL,
-    vehicle_type varchar(20) NOT NULL,
-    year_s varchar(9) NOT NULL,
-    price decimal(8, 2) NOT NULL,
-    stock_level int(4) NOT NULL,
-    stock_level_threshold int(4) NOT NULL,
-    PRIMARY KEY (id_part),
-    UNIQUE INDEX (id_part)
+CREATE TABLE `Parts` (
+    `id_part` int NOT NULL AUTO_INCREMENT,
+    `part_name` varchar(200) NOT NULL,
+    `part_type` varchar(50),
+    `code` varchar(25) NOT NULL,
+    `manufacturer` varchar(20) NOT NULL,
+    `vehicle_type` varchar(20) NOT NULL,
+    `year_s` varchar(9) NOT NULL,
+    `price` decimal(8, 2) NOT NULL,
+    `stock_level` int NOT NULL,
+    `stock_level_threshold` int NOT NULL,
+    PRIMARY KEY (`id_part`),
+    UNIQUE INDEX (`id_part`)
 );
 
-CREATE TABLE Vehicles (
-    id_reg_no varchar(10) NOT NULL,
-    manufacturer varchar(20) NOT NULL,
-    model varchar(50) NOT NULL,
-    engine_serial_number varchar(9) NOT NULL UNIQUE,
-    chassis_number varchar(8) NOT NULL UNIQUE,
-    colour varchar(15) NOT NULL,
-    last_mot datetime NOT NULL,
-    PRIMARY KEY (id_reg_no),
-    UNIQUE INDEX (id_reg_no)
+CREATE TABLE `Vehicles` (
+    `id_reg_no` varchar(10) NOT NULL,
+    `manufacturer` varchar(20) NOT NULL,
+    `model` varchar(50) NOT NULL,
+    `engine_serial_number` varchar(9) NOT NULL UNIQUE,
+    `chassis_number` varchar(8) NOT NULL UNIQUE,
+    `colour` varchar(15) NOT NULL,
+    `last_mot` date NOT NULL,
+    PRIMARY KEY (`id_reg_no`),
+    UNIQUE INDEX (`id_reg_no`)
 );
 
-CREATE TABLE Jobs_Customers (
-    job_id int(11) NOT NULL,
-    customer_id int(11) NOT NULL
+CREATE TABLE `Jobs_Customers` (
+    `job_id` int NOT NULL,
+    `customer_id` int NOT NULL
 );
 
-CREATE TABLE Users_Jobs (
-    user_id int(11) NOT NULL,
-    job_id int(11) NOT NULL
+CREATE TABLE `Users_Jobs` (
+    `user_id` int NOT NULL,
+    `job_id` int NOT NULL
 );
 
-CREATE TABLE Services (
-    id_service int(11) NOT NULL AUTO_INCREMENT,
-    service_name varchar(255) NOT NULL,
-    service_price decimal(8, 2) NOT NULL,
-    approx_time_min int(4) NOT NULL,
-    PRIMARY KEY (id_service),
-    UNIQUE INDEX (id_service)
+CREATE TABLE `Services` (
+    `id_service` int NOT NULL AUTO_INCREMENT,
+    `service_name` varchar(255) NOT NULL,
+    `service_price` decimal(8, 2) NOT NULL,
+    `approx_time_min` int NOT NULL,
+    `short_description` varchar(100) NOT NULL,
+    PRIMARY KEY (`id_service`),
+    UNIQUE INDEX (`id_service`)
 );
 
-CREATE TABLE Jobs_Services (
-    job_id int(11) NOT NULL,
-    service_id int(11) NOT NULL
+CREATE TABLE `Jobs_Services` (
+    `job_id` int NOT NULL,
+    `service_id` int NOT NULL
 );
 
-CREATE TABLE Jobs_Parts (
-    part_id int(11) NOT NULL,
-    job_id int(11) NOT NULL,
-    quantity_used int(4)
+CREATE TABLE `Jobs_Parts` (
+    `part_id` int NOT NULL,
+    `job_id` int NOT NULL,
+    `quantity_used` int
 );
 
-CREATE TABLE Customers_Vehicles (
-    customer_id int(11) NOT NULL,
-    reg_no_id varchar(10) NOT NULL
+CREATE TABLE `Customers_Vehicles` (
+    `customer_id` int NOT NULL,
+    `reg_no_id` varchar(10) NOT NULL
 );
 
-CREATE TABLE RetailSales (
-    id_sale int(11) NOT NULL AUTO_INCREMENT,
-    sale_date datetime NOT NULL,
-    sale_amount decimal(8, 2) NOT NULL,
-    PRIMARY KEY (id_sale),
-    UNIQUE INDEX (id_sale)
+CREATE TABLE `Parts_Payments` (
+    `part_id` int NOT NULL,
+    `quantity_sold` int,
+    `payment_id` int NOT NULL
 );
 
-CREATE TABLE Parts_RetailSales (
-    part_id int(11) NOT NULL,
-    sale_id int(11) NOT NULL,
-    quantity_sold int(4)
+CREATE TABLE `Payments_Customer` (
+    `payment_id` int NOT NULL,
+    `customer_id` int NOT NULL
 );
 
-CREATE TABLE Payments_Customer (
-    payment_id int(10) NOT NULL,
-    customer_id int(11) NOT NULL
+CREATE TABLE `Payments` (
+    `id_payment` int NOT NULL AUTO_INCREMENT,
+    `cash_or_card` varchar(4) NOT NULL,
+    `amount` decimal(8, 2) NOT NULL,
+    `create_date` timestamp NOT NULL,
+    `payment_date` date NULL,
+    `payment_due` date NULL,
+    PRIMARY KEY (`id_payment`),
+    UNIQUE INDEX (`id_payment`)
 );
 
-CREATE TABLE Payments (
-    id_payment int(10) NOT NULL AUTO_INCREMENT,
-    cash_or_card varchar(4) NOT NULL,
-    amount decimal(8, 2) NOT NULL,
-    create_date timestamp NOT NULL,
-    payment_date datetime NULL,
-    payment_due datetime NULL,
-    PRIMARY KEY (id_payment),
-    UNIQUE INDEX (id_payment)
+CREATE TABLE `Orders` (
+    `id_order` int NOT NULL AUTO_INCREMENT,
+    `status` varchar(20) DEFAULT 'pending' NOT NULL,
+    `description` varchar(50) NOT NULL,
+    `order_date` date NOT NULL,
+    `order_arrival` date,
+    `order_amount` decimal(8, 2) NOT NULL,
+    PRIMARY KEY (`id_order`),
+    UNIQUE INDEX (`id_order`)
 );
 
-CREATE TABLE Orders (
-    id_order int(11) NOT NULL AUTO_INCREMENT,
-    status varchar(20) DEFAULT 'pending' NOT NULL,
-    order_date datetime NOT NULL,
-    order_amount decimal(8, 2) NOT NULL,
-    PRIMARY KEY (id_order),
-    UNIQUE INDEX (id_order)
+CREATE TABLE `Parts_Orders` (
+    `part_id` int NOT NULL,
+    `order_id` int NOT NULL,
+    `quantity_ordered` int NOT NULL
 );
 
-CREATE TABLE Parts_Orders (
-    part_id int(11) NOT NULL,
-    order_id int(11) NOT NULL,
-    descrption varchar(50) NOT NULL,
-    quantity_ordered int(4) NOT NULL,
-    price decimal(8, 2)
+CREATE TABLE `Customer_Variable_Discounts_Services` (
+    `customer_id` int NOT NULL,
+    `service_id` int NOT NULL,
+    `discount` int NOT NULL
 );
 
-CREATE TABLE Customer_Variable_Discounts_Services (
-    customer_id int(11) NOT NULL,
-    service_id int(11) NOT NULL,
-    discount int(2) NOT NULL
+CREATE TABLE `Jobs_Payments` (
+    `Job_ID` int NOT NULL,
+    `Payment_ID` int NOT NULL
 );
 
 ALTER TABLE
-    Users_Roles
+    `Users_Roles`
 ADD
-    CONSTRAINT FKUsers_Role251675 FOREIGN KEY (user_id) REFERENCES Users (id_user);
+    CONSTRAINT `FKUsers_Role251675` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id_user`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Users_Roles
+    `Users_Roles`
 ADD
-    CONSTRAINT FKUsers_Role790442 FOREIGN KEY (role_id) REFERENCES Roles (id_role);
+    CONSTRAINT `FKUsers_Role790442` FOREIGN KEY (`role_id`) REFERENCES `Roles` (`id_role`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs_Customers
+    `Jobs_Customers`
 ADD
-    CONSTRAINT FKJobs_Custo162114 FOREIGN KEY (job_id) REFERENCES Jobs (id_job);
+    CONSTRAINT `FKJobs_Custo162114` FOREIGN KEY (`job_id`) REFERENCES `Jobs` (`id_job`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs_Customers
+    `Jobs_Customers`
 ADD
-    CONSTRAINT FKJobs_Custo598651 FOREIGN KEY (customer_id) REFERENCES Customers (id_customer);
+    CONSTRAINT `FKJobs_Custo598651` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`id_customer`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Users_Jobs
+    `Users_Jobs`
 ADD
-    CONSTRAINT FKUsers_Jobs928384 FOREIGN KEY (user_id) REFERENCES Users (id_user);
+    CONSTRAINT `FKUsers_Jobs928384` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id_user`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Users_Jobs
+    `Users_Jobs`
 ADD
-    CONSTRAINT FKUsers_Jobs91376 FOREIGN KEY (job_id) REFERENCES Jobs (id_job);
+    CONSTRAINT `FKUsers_Jobs91376` FOREIGN KEY (`job_id`) REFERENCES `Jobs` (`id_job`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs_Services
+    `Jobs_Services`
 ADD
-    CONSTRAINT FKJobs_Servi182926 FOREIGN KEY (job_id) REFERENCES Jobs (id_job);
+    CONSTRAINT `FKJobs_Servi182926` FOREIGN KEY (`job_id`) REFERENCES `Jobs` (`id_job`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs_Services
+    `Jobs_Services`
 ADD
-    CONSTRAINT FKJobs_Servi475550 FOREIGN KEY (service_id) REFERENCES Services (id_service);
+    CONSTRAINT `FKJobs_Servi475550` FOREIGN KEY (`service_id`) REFERENCES `Services` (`id_service`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs_Parts
+    `Jobs_Parts`
 ADD
-    CONSTRAINT FKJobs_Parts634881 FOREIGN KEY (part_id) REFERENCES Parts (id_part);
+    CONSTRAINT `FKJobs_Parts634881` FOREIGN KEY (`part_id`) REFERENCES `Parts` (`id_part`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs_Parts
+    `Jobs_Parts`
 ADD
-    CONSTRAINT FKJobs_Parts55459 FOREIGN KEY (job_id) REFERENCES Jobs (id_job);
+    CONSTRAINT `FKJobs_Parts55459` FOREIGN KEY (`job_id`) REFERENCES `Jobs` (`id_job`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Customers_Vehicles
+    `Customers_Vehicles`
 ADD
-    CONSTRAINT FKCustomers_94687 FOREIGN KEY (customer_id) REFERENCES Customers (id_customer);
+    CONSTRAINT `FKCustomers_94687` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`id_customer`);
 
 ALTER TABLE
-    Customers_Vehicles
+    `Customers_Vehicles`
 ADD
-    CONSTRAINT FKCustomers_547421 FOREIGN KEY (reg_no_id) REFERENCES Vehicles (id_reg_no);
+    CONSTRAINT `FKCustomers_547421` FOREIGN KEY (`reg_no_id`) REFERENCES `Vehicles` (`id_reg_no`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Parts_RetailSales
+    `Payments_Customer`
 ADD
-    CONSTRAINT FKParts_Reta997959 FOREIGN KEY (part_id) REFERENCES Parts (id_part);
+    CONSTRAINT `FKPayments_C594261` FOREIGN KEY (`payment_id`) REFERENCES `Payments` (`id_payment`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Parts_RetailSales
+    `Payments_Customer`
 ADD
-    CONSTRAINT FKParts_Reta600423 FOREIGN KEY (sale_id) REFERENCES RetailSales (id_sale);
+    CONSTRAINT `FKPayments_C52864` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`id_customer`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Payments_Customer
+    `Parts_Orders`
 ADD
-    CONSTRAINT FKPayments_C594261 FOREIGN KEY (payment_id) REFERENCES Payments (id_payment);
+    CONSTRAINT `FKParts_Orde7122` FOREIGN KEY (`part_id`) REFERENCES `Parts` (`id_part`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Payments_Customer
+    `Parts_Orders`
 ADD
-    CONSTRAINT FKPayments_C52864 FOREIGN KEY (customer_id) REFERENCES Customers (id_customer);
+    CONSTRAINT `FKParts_Orde56004` FOREIGN KEY (`order_id`) REFERENCES `Orders` (`id_order`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs
+    `Jobs`
 ADD
-    CONSTRAINT FKJobs204816 FOREIGN KEY (payment_id) REFERENCES Payments (id_payment);
+    CONSTRAINT `FKJobs210684` FOREIGN KEY (`reg_no_id`) REFERENCES `Vehicles` (`id_reg_no`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Parts_Orders
+    `Customer_Variable_Discounts_Services`
 ADD
-    CONSTRAINT FKParts_Orde7122 FOREIGN KEY (part_id) REFERENCES Parts (id_part);
+    CONSTRAINT `FKCustomer_V790173` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`id_customer`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Parts_Orders
+    `Customer_Variable_Discounts_Services`
 ADD
-    CONSTRAINT FKParts_Orde56004 FOREIGN KEY (order_id) REFERENCES Orders (id_order) ON DELETE CASCADE;
+    CONSTRAINT `FKCustomer_V539825` FOREIGN KEY (`service_id`) REFERENCES `Services` (`id_service`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Jobs
+    `Jobs_Payments`
 ADD
-    CONSTRAINT FKJobs638019 FOREIGN KEY (no_reg_id) REFERENCES Vehicles (id_reg_no);
+    CONSTRAINT `FKJobs_Payme844591` FOREIGN KEY (`Job_ID`) REFERENCES `Jobs` (`id_job`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Customer_Variable_Discounts_Services
+    `Jobs_Payments`
 ADD
-    CONSTRAINT FKCustomer_V790173 FOREIGN KEY (customer_id) REFERENCES Customers (id_customer);
+    CONSTRAINT `FKJobs_Payme733948` FOREIGN KEY (`Payment_ID`) REFERENCES `Payments` (`id_payment`) ON UPDATE CASCADE;
 
 ALTER TABLE
-    Customer_Variable_Discounts_Services
+    `Parts_Payments`
 ADD
-    CONSTRAINT FKCustomer_V539825 FOREIGN KEY (service_id) REFERENCES Services (id_service);
+    CONSTRAINT `FKParts_Paym554541` FOREIGN KEY (`part_id`) REFERENCES `Parts` (`id_part`) ON UPDATE CASCADE;
+
+ALTER TABLE
+    `Parts_Payments`
+ADD
+    CONSTRAINT `FKParts_Paym813726` FOREIGN KEY (`payment_id`) REFERENCES `Payments` (`id_payment`) ON UPDATE CASCADE;
--- garits_db/queries_first_deliverable.sql
+++ garits_db/queries_first_deliverable.sql
@@ -1,21 +1,166 @@
+USE garits;
+
 -- This file contains queries for the ER Diagram required in the first deliverable of the project. (2 Selects, 2 Inserts, 2 Updates, 2 Deletes and 2 Reporting queries based on the scenario)
 -- Select 1
-SELECT c.name AS 'Customer', CONCAT(v.manufacturer,' ',v.model) AS 'Car', v.id_reg_no AS 'Registration Number' from customers c inner join customers_vehicles cv on cv.customer_id=c.id_customer inner join vehicles v on v.id_reg_no=cv.reg_no_id order by 1,2;
+SELECT
+    c.name AS 'Customer',
+    CONCAT(v.manufacturer, ' ', v.model) AS 'Car',
+    v.id_reg_no AS 'Registration Number'
+from
+    customers c
+    inner join customers_vehicles cv on cv.customer_id = c.id_customer
+    inner join vehicles v on v.id_reg_no = cv.reg_no_id
+order by
+    1,
+    2;
+
 -- Select 2
-SELECT part_name AS 'Part',part_type AS 'Type', code 'Part code', manufacturer AS 'Car Manufacturer', price AS 'Price', stock_level AS 'Quantity', ROUND(price*stock_level) AS 'Stock cost' FROM parts order by 'Stock cost';
+SELECT
+    part_name AS 'Part',
+    part_type AS 'Type',
+    code 'Part code',
+    manufacturer AS 'Car Manufacturer',
+    price AS 'Price',
+    stock_level AS 'Quantity',
+    ROUND(price * stock_level) AS 'Stock cost'
+FROM
+    parts
+order by
+    'Stock cost';
+
 -- Update 1
+UPDATE
+    customers
+SET
+    fixed_discount = 5,
+    flex_discounts_json = "{{ rangeFrom: 5000, discount: 5 },{ rangeFrom: 10000, discount: 10 }}"
+WHERE
+    id_customer = 1;
 
 -- Update 2
+UPDATE
+    parts
+SET
+    price = price * 1.1
+WHERE
+    manufacturer = 'Hyundai';
 
 -- Insert 1
-INSERT INTO Parts ( part_name, part_type, code, manufacturer, vehicle_type, year_s, price, stock_level, stock_level_threshold) VALUES ( 'Airtex Water Block', 'Water Pump', '475861', 'Hyundai', 'Tucson', 2020, 36.00, 12, 4);
+INSERT INTO
+    parts (
+        part_name,
+        part_type,
+        code,
+        manufacturer,
+        vehicle_type,
+        year_s,
+        price,
+        stock_level,
+        stock_level_threshold
+    )
+VALUES
+    (
+        'Airtex Water Block',
+        'Water Pump',
+        '475861',
+        'Hyundai',
+        'Tucson',
+        2020,
+        36.00,
+        12,
+        4
+    );
+
 -- Insert 2
-INSERT INTO Customers (name, address,city,postcode,telephone_number,email,fax,fixed_discount,flex_discounts_json) VALUES ('The Car Company','Top Gear Street 17','London','E1 15ER','1234567890','car_company@gmail.com',null,7,"{{ rangeFrom: 10000, discount: 5 },{ rangeFrom: 20000, discount: 10 }}");
+INSERT INTO
+    customers (
+        name,
+        address,
+        city,
+        postcode,
+        telephone_number,
+        email,
+        fax,
+        fixed_discount,
+        flex_discounts_json
+    )
+VALUES
+    (
+        'The Car Company',
+        'Top Gear Street 17',
+        'London',
+        'E1 15ER',
+        '1234567890',
+        'car_company@gmail.com',
+        null,
+        7,
+        "{{ rangeFrom: 10000, discount: 5 },{ rangeFrom: 20000, discount: 10 }}"
+    );
+
 -- Delete 1 -  Deleting overpriced services (services that cost >1000)
-DELETE FROM services where service_price>1000;
+DELETE FROM
+    services
+where
+    service_price > 1000;
+
 -- Delete 2 - Deleting cancelled orders
-DELETE FROM orders where status='CANCELLED';
+DELETE FROM
+    orders
+where
+    status = 'CANCELLED';
+
 -- Report 1 (Job Sheet)
-
+-- SELECT c.name,c.address,c.city,c.postcode
 -- Report 2 (Spare Parts / Stock Level)
+SELECT
+    p.part_name AS 'Part Name',
+    p.code AS 'Part Code',
+    p.manufacturer AS 'Manufacturer',
+    p.vehicle_type AS 'Vehicle Type',
+    p.year_s AS 'Year(s)',
+    ROUND(p.price, 2) AS 'Price',
+    p.stock_level AS 'Initial Stock level',
+    ROUND(p.price * p.stock_level, 2) AS 'Initial cost, £',
+    jp.quantity_used AS 'Used',
+    po.quantity_ordered AS 'Delivery',
+    (
+        p.stock_level + po.quantity_ordered - jp.quantity_used
+    ) AS 'New Stock level',
+    ROUND(
+        (
+            p.stock_level + po.quantity_ordered - jp.quantity_used
+        ) * p.price,
+        2
+    ) AS 'Stock cost,£',
+    p.stock_level_threshold AS 'Low level Threshold'
+FROM
+    parts p
+    LEFT JOIN parts_orders po ON po.part_id = p.id_part
+    LEFT JOIN orders o ON o.id_order = po.order_id
+     LEFT JOIN jobs_parts jp ON p.id_part = jp.part_id
+     LEFT JOIN jobs j ON j.id_job = jp.job_id
+WHERE
+    o.order_arrival = CURDATE()
+    AND j.fix_date = CURDATE()
+    AND o.status='completed'
+    AND j.status='completed'
+UNION
+SELECT 'Total','','','','','','',SUM(p.price*p.stock_level),'','','',ROUND((p.stock_level + po.quantity_ordered - jp.quantity_used) * p.price,2),''
+FROM parts p
+    LEFT JOIN parts_orders po ON po.part_id = p.id_part
+    LEFT JOIN orders o ON o.id_order = po.order_id
+     LEFT JOIN jobs_parts jp ON p.id_part = jp.part_id
+     LEFT JOIN jobs j ON j.id_job = jp.job_id
+WHERE
+    o.order_arrival = CURDATE()
+    AND j.fix_date = CURDATE()
+    AND o.status='completed'
+    AND j.status='completed';
 
+select * from parts_orders;
+/*select * from jobs;
+INSERT INTO orders (status,description,order_date,order_arrival,order_amount) VALUES('completed','test','2022-03-02','2022-03-05',100);
+insert into parts_orders (part_id,order_id,quantity_ordered) VALUES(1,1,3);
+insert into jobs(reg_no_id,status, est_time_min,act_time_min,fix_date,create_date) VALUES('123','completed',123,321,'2022-03-05',curdate());
+insert into jobs_parts (part_id,job_id,quantity_used) values(1,2,2);
+insert into vehicles (id_reg_no,manufacturer,model,engine_serial_number,chassis_number, colour,last_mot) VALUES('123','Hyundai','Tucson','654324','532532','red','2022-03-01');*/
