2af1baf5bf59366c771183d00143502ec05af1b5
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-03-13 15:07:10 +0000

    Created service package

---
+++ garits/src/main/java/com/garits/service/Service.java
@@ -0,0 +1,12 @@
+package com.garits.service;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="services")
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_service")
+    private Integer idService;
+}
