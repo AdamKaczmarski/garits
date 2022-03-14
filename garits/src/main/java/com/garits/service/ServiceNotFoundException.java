f17116547fcfb5c5e0c27907c2687978edbdfc19
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-03-14 21:57:51 +0000

    Delete ServiceNotFoundException.java

--- garits/src/main/java/com/garits/service/ServiceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.garits.service;
-
-public class ServiceNotFoundException extends RuntimeException{
-    ServiceNotFoundException(Integer id){
-        super("Could not find service " + id);
-    }
-}
