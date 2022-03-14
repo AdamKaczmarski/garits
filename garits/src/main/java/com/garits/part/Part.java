dc59a66f29757837d21bc4c8ed8e2044171e938b
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-03-14 10:18:35 +0000

    Part entity

--- garits/src/main/java/com/garits/part/Part.java
+++ garits/src/main/java/com/garits/part/Part.java
@@ -9,4 +9,98 @@ public class Part {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_part")
     private Integer idPart;
+    @Column(name="part_name")
+    private String partName;
+    @Column(name="part_type")
+    private String partType;
+    @Column(name="code")
+    private String code;
+    @Column(name = "manufacturer")
+    private String manufacturer;
+    @Column(name="vehicle_type")
+    private String vehicleType;
+    @Column(name="year_s")
+    private String yearS;
+    @Column(name="price")
+    private double price;
+    @Column(name="stock_level")
+    private int stockLevel;
+    @Column(name="stock_level_threshold")
+    private int stockLevelThreshold;
+
+    public Integer getIdPart() {
+        return idPart;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public String getPartType() {
+        return partType;
+    }
+
+    public void setPartType(String partType) {
+        this.partType = partType;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getYearS() {
+        return yearS;
+    }
+
+    public void setYearS(String yearS) {
+        this.yearS = yearS;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockLevel() {
+        return stockLevel;
+    }
+
+    public void setStockLevel(int stockLevel) {
+        this.stockLevel = stockLevel;
+    }
+
+    public int getStockLevelThreshold() {
+        return stockLevelThreshold;
+    }
+
+    public void setStockLevelThreshold(int stockLevelThreshold) {
+        this.stockLevelThreshold = stockLevelThreshold;
+    }
 }
