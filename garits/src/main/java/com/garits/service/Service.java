312bd63e514c24847b4aace5b6cea88299134a3f
adz912 <71795879+adz912@users.noreply.github.com>
2022-03-14 20:51:15 +0000

    Added the fields and getters/setters.

--- garits/src/main/java/com/garits/service/Service.java
+++ garits/src/main/java/com/garits/service/Service.java
@@ -4,9 +4,45 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "services")
+//Fields
 public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_service")
     private Integer idService;
+    @Column(name = "service_name")
+    private String serviceName;
+    @Column(name = "service_price")
+    private double serivcePrice;
+    @Column(name = "approx_time_min")
+    private int approxTimeMin;
+    
+    //Getters and Setters
+    public Integer getIdService() {
+        return idService;
+    }
+    
+    public String getServiceName() {
+        return serviceName;
+    }
+    
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+    
+    public double getServicePrice() {
+        return servicePrice;
+    }
+    
+    public void setServicePrice(double servicePrice) {
+        this.servicePrice = servicePrice;
+    }
+    
+    public int getApproxTimeMin() {
+        return approxTimeMin;
+    }
+    
+    public void setApproxTimeMin(int approxTimeMin) {
+        this.approxTimeMin = approxTimeMin;
+    }
 }
