7d757f9f3dfd5b5bad5d6b656592cbe1425d12b2
adz912 <Adel.Zaky@city.ac.uk>
2022-03-28 23:46:06 +0100

    Update ServiceRepository.java

--- garits/src/main/java/com/garits/service/ServiceRepository.java
+++ garits/src/main/java/com/garits/service/ServiceRepository.java
@@ -1,12 +1,6 @@
 package com.garits.service;
 
-import com.garits.service.Service;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import javax.transaction.Transactional;
 
 public interface ServiceRepository extends CrudRepository<Service, Integer> {
 }
