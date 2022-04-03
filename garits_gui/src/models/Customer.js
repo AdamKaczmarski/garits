90f89ca2e35e02057adab6e91afafcd33ea7d110
AdamKaczmarski <adam.kaczmarski@city.ac.uk>
2022-04-03 21:38:13 +0100

    Create Customer.js

---
+++ garits_gui/src/models/Customer.js
@@ -0,0 +1,16 @@
+export default class Customer  {
+    constructor(){
+      this.idCustomer=0;
+      this.name= "";
+      this.address= "";
+      this.postcode= "";
+      this.city="";
+      this.telephoneNumber= "";
+      this.email= "";
+      this.fax= null;
+      this.fixedDiscount= 0;
+      this.variableDiscounts=[];
+      this.flexDiscounts=[];
+    }
+  }
+  
